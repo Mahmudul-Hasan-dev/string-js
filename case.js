73d0237f68afcf851db66c6131fc86fa1b836f79
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-08 20:41:04 +0600

    case done

--- case.js
+++ case.js
@@ -2,3 +2,9 @@ const userName = 'blackPink';
 const userInput = 'BlackPink';
 console.log(userName.toLowerCase());
 console.log(userInput.toLowerCase());
+if (userName.toLowerCase() == userInput.toLowerCase()) {
+    console.log('valid User');
+}
+else {
+    console.log('invalid user');
+}
