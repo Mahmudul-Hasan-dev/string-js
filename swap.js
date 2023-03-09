21f1cb4ad3804b6d5c72a155262f2eb5f344fe1c
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-09 07:53:56 +0600

    completed

--- swap.js
+++ swap.js
@@ -4,8 +4,13 @@ var first = 10;
 var second = 13;
 console.log(first, second);
 
+//approach 1
 //use temp to store the current value 
 var temp = first;
 first = second;
 second = temp;
 console.log(first, second);
+
+//approach 2:Destructuring
+[first, second] = [second, first];
+console.log('approach 2:', first, second);
